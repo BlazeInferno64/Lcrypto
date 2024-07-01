a8b5c3208d3339587e92a4a9edfbe711f048ac37
BlazeInferno64 <96913755+BlazeInferno64@users.noreply.github.com>
2024-07-01 13:12:21 +0530

    Add files via upload

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -5,18 +5,38 @@ img.addEventListener("contextmenu", (e) => {
     e.preventDefault();
 })
 
+let url;
+
+const lcryptoURL = 'https://raw.githubusercontent.com/BlazeInferno64/Lcrypto/main/dist/Lcrypto.exe';
+const lcryptoDecryptURL = 'https://raw.githubusercontent.com/BlazeInferno64/Lcrypto/main/dist/Lcrypto-decrypt.exe';
+
 document.addEventListener("DOMContentLoaded", (e) => {
     console.log(`Welcome to Lcrypto's homepage!`)
 })
 
-const downloadLcrypto = async() => {
-    alert(`Downloaing Lcrypto.exe...`);
-    window.location.href = `https://raw.githubusercontent.com/BlazeInferno64/Lcrypto/main/dist/Lcrypto.exe`;
+const downloadFiles = () => {
+    url = lcryptoURL;
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.download = "Lcrypto-main.exe";
+    aTag.style.display = "none";
+    document.body.appendChild(aTag);
+    aTag.click();
+    document.body.removeChild(aTag);
+    console.log(`Successfully downloaded Lcrypto.exe!`);
+
+    url = lcryptoDecryptURL;
+    const aTag2 = document.createElement("a");
+    aTag2.href = url;
+    aTag.download = "Lcrypto-decrypt.exe";
+    aTag2.style.display = "none";
+    document.body.appendChild(aTag2);
+    aTag2.click();
+    document.body.removeChild(aTag2);
+    console.log(`Successfully downloaded Lcrypto-decrypt.exe`);
 }
 
+
 downloadBtn.addEventListener("click",(e) => {
-    downloadLcrypto().then(() => {
-        alert(`Downloading Lcrypto-decrypt.exe...`);
-        window.location.href = "https://raw.githubusercontent.com/BlazeInferno64/Lcrypto/main/dist/Lcrypto-decrypt.exe"
-    })
+    downloadFiles();
 })
